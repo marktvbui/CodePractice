1782fd9f1cce9b214fff6e082d33a2ee8a765c95
Mark <mark.bui24@gmail.com>
2019-08-14 15:59:37 -0700

    adding the source of the question

--- persistence.js
+++ persistence.js
@@ -1,6 +1,8 @@
 /*Write a function, persistence, that takes in a positive parameter num and returns its 
 multiplicative persistence, which is the number of times you must multiply the digits in num until
-you reach a single digit. */
+you reach a single digit.
+https://www.codewars.com/kata/persistent-bugger/train/javascript
+*/
 
 function persistence(num) {
   // console.log(num);
